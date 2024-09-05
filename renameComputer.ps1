82021c628e89127723706807fc265086754e9470
Chris <chrishawkins@live.co.uk>
2024-09-05 16:07:39 +0100

    Added comments

--- renameComputer.ps1
+++ renameComputer.ps1
@@ -3,9 +3,13 @@
 # Author Chris Hawkins
 
 Clear
+
+# Get current and enter new hostname
 Write-Host "This computers hostname is: $env:COMPUTERNAME "
 $newHostname = Read-Host -prompt "Enter new hostname: "
-$yn = Read-Host -prompt "Press Y to rename $newHostname or any other key to cancel"
+
+# Confirm hostanem change
+$yn = Read-Host -prompt "Press Y to rename $newHostname this will restart the computer or any other key to cancel"
     if ($yn -eq "y" -or $yn -eq "Y") {
-         Rename-Computer -NewName $newHostname -Restart 
+         Rename-Computer -NewName $newHostname -restart 
     }
