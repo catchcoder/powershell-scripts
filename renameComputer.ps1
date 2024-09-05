15cb79c764d2db925266579d04e230926da2d6a8
Chris <chrishawkins@live.co.uk>
2024-09-05 16:00:48 +0100

    variable chnaged

--- renameComputer.ps1
+++ renameComputer.ps1
@@ -4,8 +4,8 @@
 
 Clear
 Write-Host "This computers hostname is: $env:COMPUTERNAME "
-$newName = Read-Host -prompt "Enter new hostname: "
-$yn = Read-Host -prompt "Press Y to rename $newName or any other key to cancel"
+$newHostname = Read-Host -prompt "Enter new hostname: "
+$yn = Read-Host -prompt "Press Y to rename $newHostname or any other key to cancel"
     if ($yn -eq "y" -or $yn -eq "Y") {
-         Rename-Computer -NewName $newName -Restart 
+         Rename-Computer -NewName $newHostname -Restart 
     }
